89487734678e2584a7e80e2dd6af96b3667ef4fb
reena koudi <kreena92@gmail.com>
2018-02-14 17:51:17 +0000

    Print names beginning with specific letter

--- directory.rb
+++ directory.rb
@@ -22,8 +22,10 @@ puts "The students of Villains Academy"
 puts "---------"
 end
 def print(students)
-students.each do|student|
-puts "#{student[:name]} (#{student[:cohort]} cohort)"
+students.each_with_index do|student, index|
+if student[:name].chr.upcase == "A"
+puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+end
 end
 end
 def print_footer(students)
